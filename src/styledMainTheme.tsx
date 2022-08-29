aef3b754a84f5a6ec1dd6705ab62a95e306a214b
Adrian Armesto <armestomena@gmail.com>
2022-08-29 21:28:23 +0200

    Added style colors on vars

---
+++ src/styledMainTheme.tsx
@@ -0,0 +1,12 @@
+const styledMainTheme = {
+  drakGreen: "#52616B",
+  lightGreen: "#C9D6DF",
+  white: "#ffff",
+  black: "#000000",
+  errorBackgroudColor: "#FFF3F8",
+  errorBorderColor: "#ED2E7E",
+  modalBackgroundColor: "#F2FFFB",
+  modalBorderColor: "#00BA88",
+};
+
+export default styledMainTheme;
