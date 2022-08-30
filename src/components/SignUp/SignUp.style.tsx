6eb05616075fb875d9ee1b2646b4ab848c169632
Adrian Armesto <armestomena@gmail.com>
2022-08-31 00:38:53 +0200

    Added responsive desing to sing up component

--- src/components/SignUp/SignUp.style.tsx
+++ src/components/SignUp/SignUp.style.tsx
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
 const SignUpStyle = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   width: 80%;
+  @media (min-width: 900px) {
+    flex-direction: row;
+  }
+  .form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    &__button {
+      width: 100px;
+      align-self: center;
+    }
+  }
+  .action-call {
+    font-weight: 900;
+    font-size: 2rem;
+    color: ${(props) => props.theme.drakGreen};
+    &__outstanding {
+      color: ${(props) => props.theme.lightGreen};
+    }
+  }
 `;
 
 export default SignUpStyle;
