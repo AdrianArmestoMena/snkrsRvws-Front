4433d7924d546fe89a5c58a622b0d32ecaf78c56
Adrian Armesto <armestomena@gmail.com>
2022-08-31 22:11:12 +0200

    Solved problem on use users test adding wrapper

--- src/hooks/useUsers.test.tsx
+++ src/hooks/useUsers.test.tsx
@@ -1,19 +1,47 @@
+import { renderHook } from "@testing-library/react";
 import axios from "axios";
-import { SignUp } from "../types/User";
+import Wrapper from "../test-utils/Wrapper";
+import { IUser } from "../types/User";
 import useUser from "./useUser";
 
 jest.mock("axios");
 const apiUrl = process.env.REACT_APP_API_URL;
 describe("Given a useUserApi hook", () => {
-  describe("When invoke register function with a mockUser", () => {
-    test("Then it should post a new user", async () => {
-      const mockUser: SignUp = {
+  describe("When signUp function is called with a User data", () => {
+    test("Then it should call axios post method with the given url and user", async () => {
+      const mockUser: IUser = {
         userName: "a",
         password: "a",
         email: "a",
       };
 
-      const { signUp } = useUser();
+      const {
+        result: {
+          current: { signUp },
+        },
+      } = renderHook(useUser, { wrapper: Wrapper });
+      await signUp(mockUser);
+
+      expect(axios.post).toHaveBeenCalledWith(
+        `${apiUrl}/users/signUp`,
+        mockUser
+      );
+    });
+  });
+
+  describe("When signin function is called with a UserName and a password", () => {
+    test("Then it should post a new user", async () => {
+      const mockUser: IUser = {
+        userName: "a",
+        password: "a",
+        email: "a",
+      };
+
+      const {
+        result: {
+          current: { signUp },
+        },
+      } = renderHook(useUser, { wrapper: Wrapper });
       await signUp(mockUser);
 
       expect(axios.post).toHaveBeenCalledWith(
