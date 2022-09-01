f05f119011fd250a8d01061f4f51243fe93766cb
Adrian Armesto <armestomena@gmail.com>
2022-09-01 16:33:17 +0200

    Testing log in component funcionality

--- src/components/LogIn/Login.test.tsx
+++ src/components/LogIn/Login.test.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Login from "./LogIn";
 
 const mockUseUsers = {
-  signUp: jest.fn(),
+  logIn: jest.fn(),
 };
 jest.mock("../../hooks/useUser", () => () => mockUseUsers);
 
@@ -18,6 +18,45 @@ describe("Given a FormRegister function", () => {
       });
 
       expect(heading).toBeInTheDocument();
+    });
+
+    test("And if the user click on the Submit button it should call setUser Function", async () => {
+      const users = {
+        userName: "Adrian",
+
+        password: "Adrian",
+      };
+
+      render(<Login />);
+
+      const inputUserName = screen.getByLabelText("User Name");
+      await userEvent.type(inputUserName, users.userName);
+
+      const inputPassword = screen.getByLabelText("Password");
+      await userEvent.type(inputPassword, users.password);
+
+      const button = screen.getByRole("button", { name: "Login" });
+
+      await userEvent.click(button);
+
+      expect(mockUseUsers.logIn).toHaveBeenCalled();
+    });
+
+    test("And if the user type wrong data and click on the Submit button it should call setUser Function", async () => {
+      const users = {
+        userName: "",
+        password: "",
+      };
+
+      const changeUsers = jest.fn();
+      React.useState = jest.fn().mockReturnValue([users, changeUsers]);
+
+      render(<Login />);
+
+      const button = screen.getByRole("button", { name: "Login" });
+      await userEvent.click(button);
+
+      expect(mockUseUsers.logIn).not.toHaveBeenCalled();
     });
 
     test("And if the user write in the Password input it should show", async () => {
