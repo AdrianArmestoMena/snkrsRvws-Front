a566b886e5574488297427d28e3734df55036cab
Adrian Armesto <armestomena@gmail.com>
2022-09-03 10:41:03 +0200

    Change App import on index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "@fontsource/poppins";
 import "./scss/custom.scss";
@@ -8,6 +7,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import App from "./components/App/App";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
