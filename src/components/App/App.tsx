ee0223ca32cb3983a05ecd9edb5dc68eb1bff4fc
Adrian Armesto <armestomena@gmail.com>
2022-09-03 12:57:19 +0200

    Added pointer on header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,16 @@
-<<<<<<< Updated upstream:src/components/App/App.tsx
-import React from "react";
-=======
 import React, { useEffect } from "react";
-import SignUp from "./components/SignUp/SignUp";
->>>>>>> Stashed changes:src/App.tsx
 import { Route, Routes, Navigate } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { useAppSelector } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import styledMainTheme from "../../stylesUtils/styledMainTheme";
 import Header from "../Header/Header";
 import Login from "../LogIn/LogIn";
 import ModalError from "../Modal/ModalError";
 import SignUp from "../SignUp/SignUp";
 import "./App.css";
-<<<<<<< Updated upstream:src/components/App/App.tsx
-=======
-import styledMainTheme from "./stylesUtils/styledMainTheme";
-import Login from "./components/LogIn/LogIn";
-import Header from "./components/Header/Header";
-import ModalError from "./components/Modal/ModalError";
-import { useAppDispatch, useAppSelector } from "./store/hooks";
-import { UserToken } from "./types/User";
-import { loginActionCreator } from "./store/features/users/usersSlice";
 import jwtDecode from "jwt-decode";
->>>>>>> Stashed changes:src/App.tsx
+import { loginActionCreator } from "../../store/features/users/usersSlice";
+import { UserToken } from "../../types/User";
 
 const App = () => {
   const token = localStorage.getItem("token") as string;
@@ -45,8 +32,8 @@ const App = () => {
     <ThemeProvider theme={styledMainTheme}>
       <Header />
       <div className="main-container">
-        {isOpen && <ModalError type={type} text={text} />}
         {isLoading && <ModalError type="" text="Loading..." />}
+        {isOpen && <ModalError type={type} text={text} />}
         <Routes>
           <Route path="/" element={<Navigate to="/signup" />} />
           <Route path="/signup" element={<SignUp />} />
