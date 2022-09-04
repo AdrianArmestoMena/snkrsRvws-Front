4d228375597fc87f92b5ee92c140281aaefe1018
Adrian Armesto <armestomena@gmail.com>
2022-09-04 14:34:22 +0200

    Created use reviews hook and add review function in

---
+++ src/hooks/useReviews.tsx
@@ -0,0 +1,37 @@
+import axios, { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
+import {
+  closeAllActionCreator,
+  closeLoadingActionCreator,
+  loadingUiActionCreator,
+  throwMessageErrorActionCreator,
+} from "../store/features/uiModal/uiModalSlice";
+import { useAppDispatch } from "../store/hooks";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const useReviews = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const createReview = async (formData: FormData) => {
+    let response;
+    try {
+      dispatch(loadingUiActionCreator());
+      response = await axios.post(`${apiUrl}/reviews/addreview`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch (error) {
+      const errorObject = JSON.parse((error as AxiosError).request.response);
+      dispatch(closeLoadingActionCreator());
+      dispatch(throwMessageErrorActionCreator(errorObject.error));
+      return false;
+    }
+    navigate("/");
+    dispatch(closeAllActionCreator());
+    return response.data;
+  };
+  return { createReview };
+};
+
+export default useReviews;
