feccefe2ca67127566217f157831c99ffdf3df84
Adrian Armesto <armestomena@gmail.com>
2022-09-05 13:32:10 +0200

    Tested sign up page

---
+++ src/pages/SignupPage/SignupPage.test.tsx
@@ -0,0 +1,17 @@
+import TestRenderer from "react-test-renderer";
+import Wrapper from "../../test-utils/Wrapper";
+import ReviewFormPage from "../ReviewFormPage/ReviewFormPage";
+
+describe("Given a review form Page component", () => {
+  describe("When it's instantiated", () => {
+    test("Then it should match the html from Home page", () => {
+      const expectedLoginPage = TestRenderer.create(
+        <Wrapper>
+          <ReviewFormPage />
+        </Wrapper>
+      );
+
+      expect(expectedLoginPage).toMatchSnapshot();
+    });
+  });
+});
