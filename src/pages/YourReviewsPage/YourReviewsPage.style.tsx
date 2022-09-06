1d6db06641a961a71df3fe2f2393c9fd570f22f6
Adrian Armesto <armestomena@gmail.com>
2022-09-06 23:09:00 +0200

    Added stttyle to reviews page and change some bug on card and list"

---
+++ src/pages/YourReviewsPage/YourReviewsPage.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const YourReviewsPageStyle = styled.main`
+  margin: 20px;
+  .tittle {
+    font-weight: 700;
+  }
+`;
+export default YourReviewsPageStyle;
