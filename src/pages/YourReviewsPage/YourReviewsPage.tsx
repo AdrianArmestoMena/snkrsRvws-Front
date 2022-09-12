b74559d683f66f311368bd3ac4bd40bd324dac04
Adrian Armesto <armestomena@gmail.com>
2022-09-12 15:51:51 +0200

    Added pagination to reviews list"

--- src/pages/YourReviewsPage/YourReviewsPage.tsx
+++ src/pages/YourReviewsPage/YourReviewsPage.tsx
@@ -1,11 +1,9 @@
-import FilterForm from "../../components/FilterForm/FilterForm";
 import ReviewsList from "../../components/ReviewsList/ReviewsList";
 import YourReviewsPageStyle from "./YourReviewsPage.style";
 
 const YourReviewsPage = (): JSX.Element => {
   return (
     <YourReviewsPageStyle>
-      <FilterForm />
       <h2 className="tittle">Your Reviews</h2>
       <ReviewsList isHome={false} />
     </YourReviewsPageStyle>
